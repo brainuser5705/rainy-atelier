cad6cb8987cfa2118d79c8e0f14778d405e39935
brainuser5705 <codeuser5705@outlook.com>
2025-07-14 14:30:53 -0400

    clean up code for css in prep for popup

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,5 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.querySelector('#hamburger').addEventListener('click', () => {
     document.querySelector('#landing').classList.toggle('hidden');
-    document.querySelector('.hamburger').classList.toggle('hidden');
+    document.querySelector('#hamburger').classList.toggle('hidden');
     document.querySelector('#landing-popup').classList.toggle('hidden');
 });
