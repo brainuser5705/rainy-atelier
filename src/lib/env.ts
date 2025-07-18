a525bfb8ff3263723faa9beba49f987a8ca85276
acl9213 <acl9213@rit.edu>
2025-07-17 21:30:39 -0400

    env condition fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,3 @@
-var STRAPI_URL = import.meta.env.LOCAL == true ? import.meta.env.STRAPI_LOCAL_URL : import.meta.env.STRAPI_CLOUD_URL;
+var STRAPI_URL = import.meta.env.LOCAL == "true" ? import.meta.env.STRAPI_LOCAL_URL : import.meta.env.STRAPI_CLOUD_URL;
 
 export default STRAPI_URL;
