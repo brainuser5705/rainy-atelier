37ba2e1a1da63f89289021e7b1b6f51bf8fdcf76
acl9213 <acl9213@rit.edu>
2025-07-24 14:59:52 -0400

    fix env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,5 @@
 var STRAPI_URL = import.meta.env.LOCAL == "true" ? import.meta.env.STRAPI_LOCAL_URL : import.meta.env.STRAPI_CLOUD_URL;
-// var BASE_URL = import.meta.env.LOCAL == "true" ? import.meta.env.BASE_LOCAL_URL : import.meta.env.BASE_NETLIFY_URL;
-
-var BASE_URL = import.meta.env.BASE_LOCAL_URL;
+var BASE_URL = import.meta.env.LOCAL == "true" ? import.meta.env.BASE_LOCAL_URL : import.meta.env.BASE_NETLIFY_URL;
 
 var IMG_URL_PREFIX = import.meta.env.LOCAL == "true" ? STRAPI_URL : "";
 
